f195d9e75006c23c9e3b7a1bc22e6cf77186d04b
Hyeon <Hyeon@Hyeon>
2018-11-22 16:40:22 +0900

    현;

--- BNB/src/main/java/com/bit/bnb/user/service/EncryptSHA256Service.java
+++ BNB/src/main/java/com/bit/bnb/user/service/EncryptSHA256Service.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EncryptSHA256Service {
-
+  
 	public static String encrypt(String planText) {
 		try {
 			MessageDigest md = MessageDigest.getInstance("SHA-256");
